6c94ae0a57869bb08211ef0d97759d4e7675aac3
brtlvrs <bart@bartlievers.nl>
2021-04-14 10:36:47 +0200

    temp added

---
+++ deploy/deploy.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+## create pwcli wrapper to run the pwcli container interactively
+
+cat > /usr/local/bin/pwcli << EOF
+#!/usr/bin/env bash
+#
+#  A script wrapper for running the docker image pwcli interactively
+#
+
+docker_img="brtlvrs/pwcli:latest"
+
+docker run --rm -it \
+  -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
+  -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
+  -v $(pwd):/pwsh \
+ $docker_img pwsh "$@"
+EOF
+
+chmod +x /usr/local/bin/pwcli
